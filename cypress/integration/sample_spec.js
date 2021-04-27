3ed4113b84ce46cb9a511fa75dca464b9d66dca5
Tom Shetler <tshetler@milestechnologies.com>
2021-04-26 20:43:57 -0400

    Add more tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,8 +3,31 @@ describe("Google Search Page", () => {
     cy.visit("http://127.0.0.1:5500/google.html");
   });
 
+  it("Should have an exernal CSS file", () => {
+    cy.get("link").invoke("attr", "href").should("eq", "google.css");
+  });
+
   it("Should have Google title", () => {
     cy.title().should("eq", "Google");
+  });
+
+  it("Should have an input", () => {
+    let element = cy.get("input[type=text]");
+    element.invoke("attr", "type").should("eq", "text");
+  });
+
+  // Focus test not working
+  // it("Should have the input defaulted to focus", () => {
+  //   cy.focused().should("have.attr", "name", "search");
+  // });
+
+  it("Should have Google logo", () => {
+    cy.get(".google-image")
+      .invoke("attr", "src")
+      .should(
+        "eq",
+        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+      );
   });
 
   it("Should have Google Search button", () => {
@@ -17,5 +40,33 @@ describe("Google Search Page", () => {
       .first()
       .next()
       .contains("I'm Feeling Lucky");
+  });
+
+  it("Should have the appropriate left footer nav links", () => {
+    cy.get(".left").children().should("have.length", 3);
+  });
+
+  it("Should have the Advertising footer nav link", () => {
+    cy.get(".left").children().first().contains("Advertising");
+  });
+
+  it("Should have the Business footer nav link", () => {
+    cy.get(".left").children().first().next().contains("Business");
+  });
+
+  it("Should have the How Search works footer nav link", () => {
+    cy.get(".left").children().last().contains("How Search works");
+  });
+
+  it("Should have the Privacy footer nav link", () => {
+    cy.get(".right").children().first().contains("Privacy");
+  });
+
+  it("Should have the Terms footer nav link", () => {
+    cy.get(".right").children().first().next().contains("Terms");
+  });
+
+  it("Should have the Settings footer nav link", () => {
+    cy.get(".right").children().last().contains("Settings");
   });
 });
