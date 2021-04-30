5061a0d46f23bf13fc01440c0bdb6f71aae936ba
Johan Burke <jburke@milestechnologies.com>
2021-04-30 15:33:10 -0400

    added 200 on root route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = 3000;
 
 app.use(express.static("site"));
 
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+
 app.listen(port, () => {
     console.log(`Simple server listening at http://localhost:${port}`);
 });
